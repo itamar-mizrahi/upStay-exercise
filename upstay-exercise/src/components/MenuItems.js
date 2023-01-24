74f67c699990dc587947ac385038a81cecdb4a2d
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2023-01-24 14:26:54 +0200

    clean code

--- upstay-exercise/src/components/MenuItems.js
+++ upstay-exercise/src/components/MenuItems.js
@@ -17,7 +17,6 @@ const MenuItems = ({ items, depthLevel }) => {
     document.addEventListener("mousedown", handler);
     document.addEventListener("touchstart", handler);
     return () => {
-      // Cleanup the event listener
       document.removeEventListener("mousedown", handler);
       document.removeEventListener("touchstart", handler);
     };
